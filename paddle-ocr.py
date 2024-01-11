3eddc9d05fa8638373306456f6f7bce023d73d0d
DESKTOP-JEHT1NC\ggoch2 <ggoch>
2024-01-11 23:32:54 +0800

    feat: update video play

--- paddle-ocr.py
+++ paddle-ocr.py
@@ -5,7 +5,9 @@ import cv2
 import numpy as np
 import time
 import fps_plot
+import show_fn
 import torch
+from video_player import VideoPlayer
 
 from paddleocr import PaddleOCR, draw_ocr
 
@@ -16,20 +18,9 @@ from collections import defaultdict
 
 ocr = PaddleOCR(use_angle_cls=True, lang="ch")  # need to run only once to download and load model into memory
 
+model = YOLO("models/first-train/weights/best.pt")  # load a custom model
 
-def text(img, text, xy=(0, 0), color=(0, 0, 0), size=20, stroke_width=2):
-    pil = Image.fromarray(img)
-    s = platform.system()
-
-    if s == "Linux":
-        font = ImageFont.truetype("/usr/share/fonts/truetype/wqy/wqy-zenhei.ttc", size)
-    elif s == "Darwin":
-        font = ImageFont.truetype("/Library/Fonts/Arial.ttf", size)
-    else:
-        font = ImageFont.truetype("simsun.ttc", size)
-
-    ImageDraw.Draw(pil).text(xy, text, color, font=font, stroke_width=stroke_width)
-    return np.array(pil)
+video_player = VideoPlayer("datas/videos/Barataria Morvant Exit.mp4")
 
 def text_paddleocr(img):
     result = ocr.ocr(img, cls=True)[0]
@@ -44,55 +35,22 @@ def text_paddleocr(img):
     
     return texts
 
+def predict(frame):
+    img = frame[:, :, ::-1].copy()  # 转换颜色空间
+    results = model.track(img, conf=0.3, iou=0.5,persist=True)
+    boxes = results[0].boxes.xyxy
 
+    annotated_frame = results[0].plot()
 
-model = YOLO("models/first-train/weights/best.pt")  # load a custom model
+    for box in boxes:
+        x1, y1, x2, y2 = map(int, box[:4])
+        gray_img = annotated_frame[y1:y2, x1:x2].copy()
+        tmp = cv2.cvtColor(gray_img, cv2.COLOR_BGR2GRAY)
+        texts = text_paddleocr(tmp)
 
-cap = cv2.VideoCapture("datas/videos/Barataria Morvant Exit.mp4")
-# cap = cv2.VideoCapture("datas/videos/Vehicle style and Plate recolonization for moving cars.mp4")
+        for i, txt in enumerate(texts):
+            annotated_frame = show_fn.drow_text(annotated_frame, txt, (x1, y1 - 20 - i * 20), (255, 0, 0), 30)
 
-statrTime = time.time()
-frame_count = 0
-skipFrameCount = 1
+    return annotated_frame
 
-while cap.isOpened():
-    ret, frame = cap.read()
-    if not ret:
-        break
-
-    boxes = []
-
-    annotated_frame = frame[:, :, ::-1].copy()
-
-    if frame_count % skipFrameCount == 0:
-        # # 處理每一幀（以下代碼與處理單張圖片相同）
-        img = frame[:, :, ::-1].copy()  # 轉換顏色空間
-        # results = model.predict(img, save=False)
-        results = model.track(img, conf=0.3, iou=0.5,persist=True)
-        boxes = results[0].boxes.xyxy
-
-        annotated_frame = results[0].plot()
-
-        for box in boxes:
-            x1, y1, x2, y2 = map(int, box[:4])
-            gray_img = annotated_frame[y1:y2, x1:x2].copy()
-            tmp = cv2.cvtColor(gray_img, cv2.COLOR_BGR2GRAY)
-            texts = text_paddleocr(tmp)
-
-            for i, txt in enumerate(texts):
-                annotated_frame = text(annotated_frame, txt, (x1, y1 - 20 - i * 20), (255, 0, 0), 30)
-    
-    frame_count += 1
-
-    annotated_frame = fps_plot.get_fps(annotated_frame, statrTime, frame_count, (0, 0, 255))
-
-    # # 顯示結果
-    cv2.imshow('YOLO + OCR', cv2.cvtColor(annotated_frame, cv2.COLOR_RGB2BGR))
-
-    # 按 'q' 鍵退出循環
-    if cv2.waitKey(1) & 0xFF == ord("q"):
-        break
-
-# 釋放視頻捕獲對象
-cap.release()
-cv2.destroyAllWindows()
+video_player.play(predict)
